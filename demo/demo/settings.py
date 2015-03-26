75b251dc2280a2e8748ae2dd022d8fd229d0d8b4
Brian Mearns <bmearns@ieee.org>
2015-03-25 20:52:48 -0400

    Added app to settings.

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+import sys
+sys.path.insert(0, '..')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -24,8 +27,13 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates'),]
+
 ALLOWED_HOSTS = []
 
+INTERNAL_IPS = (
+    '127.0.0.1',
+)
 
 # Application definition
 
@@ -36,6 +44,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #Local
+    'agenda',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -81,3 +92,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
