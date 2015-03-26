1b723d7040a0e88e8e33fe7eea8a6f8a5cb2fc2c
Brian Mearns <bmearns@ieee.org>
2015-03-25 22:01:30 -0400

    Working on initial implementation and test.

--- agenda/models.py
+++ agenda/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Schedule(models.Model):
+    def createTask(self, title, duration):
+        task = Task(schedule=self, title=title, duration=duration)
+        task.save()
+        return task
+
+class Task(models.Model):
+    schedule = models.ForeignKey(Schedule, on_delete=models.SET_NULL, related_name='tasks')
+    title = models.CharField(max_length=255)
+    duration = models.PositiveIntegerField()
+
