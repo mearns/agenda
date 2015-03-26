5ba59c19d46b224ee26824ebbd09d0af3c8e5846
Brian Mearns <bmearns@ieee.org>
2015-03-26 09:00:52 -0400

    Updated workspace to add console for unittesting and change directory for the demo console.

--- workspace.bat
+++ workspace.bat
@@ -1,11 +1,13 @@
 
 @ECHO OFF
 
-start console -w "DEMO AGENDA" -d "%~dp0"
 start console -w "GIT AGENDA" -d "%~dp0"
+start console -w "DEMO AGENDA" -d "%~dp0\demo"
+start console -w "TEST AGENDA" -d "%~dp0\demo"
 
 start cmd /c cd "%~dp0" ^&^& start gvim -p -c ":simalt ~x"
 
 if "%PYTHON_HOME%"=="" GOTO NO_HH
     start hh "%PYTHON_HOME%.\Doc\python275.chm"
 :NO_HH
+
